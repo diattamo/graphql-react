06a8860f9e66b5ea4cce5cf31e5fadb305fa5f79
diattamo <mmddiatta@gmail.com>
2020-01-02 01:03:06 +0100

    more  of 'booking' feature

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -17,6 +17,20 @@ const user = async userId => {
   }
 };
 
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findOne(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      date: new Date(event._doc.date).toISOString(),
+      creator: user.bind(this, event.creator)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
@@ -50,13 +64,15 @@ module.exports = {
     }
   },
 
-  booking: async () => {
+  bookings: async () => {
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
         return {
           ...booking._doc,
           _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
           createdAt: new Date(booking._doc.createdAt).toISOString(),
           updatedAt: new Date(booking._doc.updatedAt).toISOString()
         };
@@ -124,6 +140,8 @@ module.exports = {
     return {
       ...result,
       _id: result.id,
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event),
       createdAt: new Date(result._doc.createdAt).toISOString(),
       updatedAt: new Date(result._doc.updatedAt).toISOString()
     };
